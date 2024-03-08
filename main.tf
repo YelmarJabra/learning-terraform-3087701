48eabb6464f97edc352dd17cce8743983972585e
YelmarJabra <142242862+YelmarJabra@users.noreply.github.com>
2024-03-08 14:49:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,10 +33,10 @@ module "blog_sg" {
   name = "blog new"
   vpc_id = data.aws_vpc.default.id
   
-  egress_rules = ["http-80-tcp, "https-443-tcp"]
+  egress_rules = ["http-80-tcp", "https-443-tcp"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
-  ingress_rules = ["http-80-tcp, "https-443-tcp"]
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
